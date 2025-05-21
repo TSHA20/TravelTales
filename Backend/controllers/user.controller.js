da271a7a7ea35a44e6147323a5d354692c31872c
Thimali Waduge <95086545+TSHA20@users.noreply.github.com>
2025-05-22 01:45:10 +0530

    Profile backend

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -48,6 +48,7 @@ exports.login = async (req, res) => {
         .cookie('token', token, {
           httpOnly: true,
           sameSite: 'Lax',
+          secure: false, 
           maxAge: 3600000
         })
         .json({
